70be0f38ca60c252e8a7ec5f84fba2f8dcd17567
Jamel Hendricks <jamel@ibm.com>
2020-07-15 01:48:19 -0400

    Template Header and SideNav

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/Home.scss";
+import { NavMenu } from "../components/NavMenu";
+
+import {} from "carbon-components-react";
+
+export class Home extends React.Component {
+	render() {
+		return <NavMenu />;
+	}
+}
